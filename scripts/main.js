802d574a5c8591cd29c38810bfdb6f45b96484ed
Max <contact@mstoiber.com>
2015-06-11 16:45:34 +0200

    Removed unnecessary docReady function, added comments.

--- scripts/main.js
+++ scripts/main.js
@@ -1,93 +1,45 @@
+// Get most common elements
 var wrapper = document.getElementsByClassName('wrapper')[0];
 var post = document.getElementsByClassName('post')[0];
 var footer = document.getElementsByClassName('footer')[0];
 
-(function(funcName, baseObj) {
-    "use strict";
-    funcName = funcName || "docReady";
-    baseObj = baseObj || window;
-    var readyList = [];
-    var readyFired = false;
-    var readyEventHandlersInstalled = false;
-    function ready() {
-        if (!readyFired) {
-            readyFired = true;
-            for (var i = 0; i < readyList.length; i++) {
-                readyList[i].fn.call(window, readyList[i].ctx);
-            }
-            readyList = [];
-        }
-    }
-
-    function readyStateChange() {
-        if ( document.readyState === "complete" ) {
-            ready();
-        }
-    }
-    baseObj[funcName] = function(callback, context) {
-        if (readyFired) {
-            setTimeout(function() {callback(context);}, 1);
-            return;
-        } else {
-            readyList.push({fn: callback, ctx: context});
-        }
-        if (document.readyState === "complete" || (!document.attachEvent && document.readyState === "interactive")) {
-            setTimeout(ready, 1);
-        } else if (!readyEventHandlersInstalled) {
-            if (document.addEventListener) {
-                document.addEventListener("DOMContentLoaded", ready, false);
-                window.addEventListener("load", ready, false);
-            } else {
-                document.attachEvent("onreadystatechange", readyStateChange);
-                window.attachEvent("onload", ready);
-            }
-            readyEventHandlersInstalled = true;
-        }
-    }
-})("docReady", window);
-
-var addEvent = function(elem, type, eventHandle) {
-    if (elem == null || typeof(elem) == 'undefined') return;
-    if ( elem.addEventListener ) {
-        elem.addEventListener( type, eventHandle, false );
-    } else if ( elem.attachEvent ) {
-        elem.attachEvent( "on" + type, eventHandle );
-    } else {
-        elem["on"+type]=eventHandle;
-    }
-};
-
-docReady(function() {
-	alignTitleBackgrounds();
-	var postlinks = document.getElementsByClassName("post-home__link");
-	for (var k = 0; k < postlinks.length; k++) {
-		addEvent(postlinks[k], "click", pageFadeOut);
+// Align the title backgrounds asap
+alignTitleBackgrounds();
+// Get all the post links and bind the click event to pageFadeOut
+var postlinks = document.getElementsByClassName("post-home__link");
+for (var k = 0; k < postlinks.length; k++) {
+	addEvent(postlinks[k], "click", pageFadeOut);
+}
+// Same thing with the title
+var titles = document.getElementsByClassName("title");
+for (var l = 0; l < titles.length; l++) {
+	addEvent(titles[l], "click", pageFadeOut);
+}
+// Then fade in the post if we're on a post page
+setTimeout(function() {
+	if (post !== undefined) {
+		post.style.transition = 'opacity 250ms ease-in-out';
+		setTimeout(function() {
+			post.style.opacity = 1;
+		}, 0);
 	}
-	var titles = document.getElementsByClassName("title");
-	for (var l = 0; l < titles.length; l++) {
-		addEvent(titles[l], "click", pageFadeOut);
+}, 0);
+// Or the wrapper if we're on any other page
+setTimeout(function() {
+	if (wrapper !== undefined) {
+		wrapper.style.transition = 'opacity 250ms ease-in-out, margin 250ms ease-in-out';
+		setTimeout(function() {
+			wrapper.style.opacity = 1;
+		}, 0);
 	}
-	setTimeout(function() {
-		if (post !== undefined) {
-			post.style.transition = 'opacity 250ms ease-in-out';
-			setTimeout(function() {
-				post.style.opacity = 1;
-			}, 0);
-		}
-	}, 0);
-	setTimeout(function() {
-		if (wrapper !== undefined) {
-			wrapper.style.transition = 'opacity 250ms ease-in-out, margin 250ms ease-in-out';
-			setTimeout(function() {
-				wrapper.style.opacity = 1;
-			}, 0);
-		}
-	}, 0);
-});
+}, 0);
 
+// Align the title backgrounds on resize
 addEvent(window, "resize", alignTitleBackgrounds);
 
+// Aligns the title backgrounds
 function alignTitleBackgrounds() {
+	// Get the backgrounds
 	var backgrounds = document.getElementsByClassName('post-home__title-bg');
 	for (var i = 0; i < backgrounds.length; i++) {
 	    var background = backgrounds[i];
@@ -95,7 +47,9 @@ function alignTitleBackgrounds() {
 
 	    for (var j = 0; j < parent.childNodes.length; j++) {
 	        if (parent.childNodes[j].className == "post-home__link") {
+	        	// Get the corresponding link to the title
 	          	var title = parent.childNodes[j];
+	          	// And position the background at the exact same spot
 	          	background.style.width = title.offsetWidth + "px";
 	          	background.style.height = title.offsetHeight + "px";
 	          	background.style.marginTop = 0 - title.offsetHeight + "px";
@@ -105,15 +59,21 @@ function alignTitleBackgrounds() {
 	}
 }
 
+// Fades out the page on click
 function pageFadeOut(evt) {
 	evt.preventDefault();
 	var target;
+	// If the click event comes from the heading
 	if (evt.target.href === undefined) {
+		// Get the href attr from the wrapping anchor tag
 		target = evt.target.parentNode.href;
 	} else {
+		// Otherwise just get the href attribute
 		target = evt.target.href;
 	}
+	// Scroll ot the top of the page
 	scrollTo(0);
+	// Fade out the page
     setTimeout(function() {
 		if (wrapper !== undefined) {
 			wrapper.style.opacity = 0;
@@ -124,12 +84,14 @@ function pageFadeOut(evt) {
 		if (footer !== undefined) {
 			footer.style.opacity = 0;
 		}
+		// Then open the clicked link
 		setTimeout(function() {
 			window.open(target,"_self");
 		}, 250);
 	}, 0);
 }
 
+// Smoothscrolls to a certain point
 function scrollTo (amount) {
 	if (amount === undefined) {
 		amount = 0;
@@ -140,4 +102,16 @@ function scrollTo (amount) {
 	} else {
 		clearTimeout(scrollTimeout);
 	}
+}
+
+// Cross browser event binding
+function addEvent(elem, type, eventHandle) {
+    if (elem == null || typeof(elem) == 'undefined') return;
+    if ( elem.addEventListener ) {
+        elem.addEventListener( type, eventHandle, false );
+    } else if ( elem.attachEvent ) {
+        elem.attachEvent( "on" + type, eventHandle );
+    } else {
+        elem["on"+type]=eventHandle;
+    }
 }
